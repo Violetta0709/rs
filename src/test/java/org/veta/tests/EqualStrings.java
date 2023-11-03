d743b6d8c07fd5a508d763979a3ab942e467b656
violetta0709 <violetta100100100@gmail.com>
2023-11-03 18:15:15 +0300

    initial commit

---
+++ src/test/java/org/veta/tests/EqualStrings.java
@@ -0,0 +1,44 @@
+package org.veta.tests;
+
+import java.util.Arrays;
+public class EqualStrings {
+    public static void main(String[] args) {
+        String [] arr = new String []{};
+        findSameLines(arr);
+    }
+
+    public static void findSameLines(String[] array) {
+        for (int i = array.length-1; i >=0; i--) {
+            String current = array[i];
+            char[] charArr = current.toCharArray();
+            Arrays.sort(charArr);
+            String sortedChars = String.valueOf(charArr);
+
+            for (int j = array.length-1 ; j > i ; j--) {
+                if (hasSameCharacters(array[j], sortedChars)) {
+                    System.out.print(sortedChars + " = " + i + ", "+ j+" ");
+
+                }
+            }
+        }
+    }
+
+    private static boolean hasSameCharacters(String charSet1, String charSet2) {
+        char[] char1 = charSet1.toCharArray();
+        char[] char2 = charSet2.toCharArray();
+
+        Arrays.sort(char1);
+        Arrays.sort(char2);
+
+        return Arrays.equals(char1, char2) && checkCase(char1) && checkCase(char2);
+    }
+
+    private static boolean checkCase (char [] chars) {
+        for (char aChar : chars) {
+            if (Character.isLowerCase(aChar))
+                return true;
+        }
+        return false;
+    }
+
+}
